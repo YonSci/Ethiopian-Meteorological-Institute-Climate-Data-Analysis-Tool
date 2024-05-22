3a40521b98890fbcc8383e726dd348a0ab791161
Yonas M <yonas.mersha14@gmail.com>
2024-05-22 14:03:54 +0300

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -1,17 +1,20 @@
+import bcrypt
 import pickle
-
 from pathlib import Path
 
-import streamlit_authenticator as stauth
-
-
-names = ["Yonas Mersha", "Teferi Demissie"]  
-
+# Example user credentials
 usernames = ["yonas", "teferi"]
-passwords = ["yonas123", "teferi123"]
+passwords = ["yon", "tef"]
 
-hashed_passwords = stauth.Hasher(passwords).generate()
+# Hash the passwords
+hashed_passwords = [bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()) for password in passwords]
 
+# Print the hashed passwords to verify
+print("Hashed Passwords:")
+for hp in hashed_passwords:
+    print(hp)
+
+# Save the hashed passwords
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
