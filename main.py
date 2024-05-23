e2e2011a54c36bc42871b35ee176aee43f2f4940
Yonas M <yonas.mersha14@gmail.com>
2024-05-23 08:40:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,66 +10,21 @@ st.set_page_config(
     layout="centered"  # wide, centered
     )
 
-# # Function to authenticate users
-# def authenticate(username, password):
-#     stored_credentials = load_user_credentials()
+import yaml
+from yaml.loader import SafeLoader
 
-#     # Check if the entered username exists in the credentials
-#     if username in stored_credentials:
-#         stored_hashed_password = stored_credentials[username]["hashed_password"]
-#         # Check if the entered password matches the stored hashed password
-#         return bcrypt.checkpw(password.encode("utf-8"), stored_hashed_password)
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-#     return False
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
 
-def authenticate(username, password):
-    stored_credentials = load_user_credentials()
 
-    # Check if the entered username exists in the credentials
-    if username in stored_credentials:
-        stored_hashed_password = stored_credentials[username]["hashed_password"]
-        # Ensure that the stored hashed password is a bytes-like object
-        if isinstance(stored_hashed_password, str):
-            stored_hashed_password = stored_hashed_password.encode("utf-8")
-        # Check if the entered password matches the stored hashed password
-        return bcrypt.checkpw(password.encode("utf-8"), stored_hashed_password)
-
-    return False
-
-# Function to load user credentials from the pickle file
-def load_user_credentials(filename="user_credentials.pkl"):
-    try:
-        with open(filename, "rb") as file:
-            return pickle.load(file)
-    except FileNotFoundError:
-        return {}
-
-# Function to create session state
-def create_session_state():
-    if "authenticated" not in st.session_state:
-        st.session_state.authenticated = False
-        
-
-# Function to reset the authentication status and rerun the script
-def logout():
-    st.session_state.authenticated = False
-    st.rerun()
-
-# Login page
-def login_page():
-    st.title(':blue[Login Page] 🔒')
-
-    username = st.text_input("Username:")
-    password = st.text_input("Password:", type="password")
-    login_button = st.button("Login")
-
-    if login_button:
-        if authenticate(username, password):
-            st.success("Login successful!")
-            st.session_state.authenticated = True
-            st.rerun()
-        else:
-            st.error("Invalid credentials. Please try again.")
     
 from app.Landing_Page import Landing_Page
 from app.Data_Importing_Module import Data_Importing_Module
