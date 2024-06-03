e4f2163eacdbd0a8b43571fc851e52b9141d161a
Yonas M <yonas.mersha14@gmail.com>
2024-06-04 01:00:47 +0300

    Update seasonal_rainfall.py

--- app/seasonal_rainfall.py
+++ app/seasonal_rainfall.py
@@ -54,7 +54,8 @@ def seasonal_rainfall():
                                         type=["nc"], key='uploaded_file1')
         if uploaded_file is not None:
         # Read the uploaded file
-            ds = xr.open_dataset(io.BytesIO(uploaded_file.read()))
+            ds = xr.open_dataset(io.BytesIO(uploaded_file.read()), engine='netcdf4')
+            # ds = xr.open_dataset(io.BytesIO(uploaded_file.read()))
             rain = ds[variable][:, :]
             lon = ds['Longitude'][:]
             lat = ds['Latitude'][:]
